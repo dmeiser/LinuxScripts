90a56446cd07975082c8d3899e82cea456e7e2db
dmeiser <dmeiser@gmail.com>
2017-01-14 11:26:41 -0500

    couple of quick fixes, added additional round of tests

--- check-hs100.sh
+++ check-hs100.sh
@@ -3,7 +3,7 @@
 # Usage:
 # Intended to be used as a cron job with flock, which is a core Linux utility.
 # I use it with the following, which will run every minute of every day:
-# * * * * * flock -n /tmp/check-hs100.lock /path/to/check-hs100.sh
+# * * * * * flock -n /tmp/check.lock /path/to/check.sh
 
 # This will find the first five hops, find rows that have IPs, take anything
 # from hop 2 to 9 (eg - past my router and modem) then grab the IP address of the
@@ -42,30 +42,49 @@ done
 
 if [ $failures -eq 3 ];
 then
-  # If /tmp/lastfailure doesn't exist, create it.
-	if ! [ -f /tmp/lastfailure ];
-	then
-		date +%s --date "7 hours ago" > /tmp/lastfailure
-	fi
+	# 1 set of tests was too gittery
+	failures=0
+	for test in "${tests[@]}";
+	do
+	        echo "Testing $test"
+	        ping -c 1 $test
 
-	# Get last failure time and find out how many minutes ago it was
-	# if it was less than 30 minutes ago, we probably have something
-	# else going on. Restart every 30 minutes until something happens.
-	let lastfailure=" (`date +%s`-`tail -n1 /tmp/lastfailure`)/60 "
-	if [ $lastfailure -ge 30 ];
+        	if [ $? -ne 0 ];
+	        then
+                	let "failures += 1"
+        	fi
+	done
+		
+	if [ $failures -eq 3];
 	then
-		date +%s >> /tmp/lastfailure
-		echo "Last failure was $lastfailure minutes ago. Restarting."
-		# Turn the HS100 off, wait 30 seconds, then turn it back on
-		# Wait 10 minutes because it takes a while for my internet
-		# to come back up.
-		# Shout out to ggeorgovassilis for the hs100.sh scripts:
-		# https://github.com/ggeorgovassilis/linuxscripts/blob/master/tp-link-hs100-smartplug/hs100.sh
-		./hs100.sh 192.168.1.60 9999 off
-		sleep 10s
-		./hs100.sh 192.168.1.60 9999 on
-	else
-		echo "Last failure was $lastfailure minutes ago. Not restarting."
+
+		# Init lastfailure with some time (so we don't fail later) if it 
+		# does not exist
+		if ! [ -f /tmp/lastfailure ];
+		then
+			date +%s --date "7 hours ago" > /tmp/lastfailure
+		fi
+
+		# Get last failure time and find out how many minutes ago it was
+		# if it was less than 30 minutes ago, we probably have something
+		# else going on. Restart every 30 minutes until something happens.
+		let lastfailure=" (`date +%s`-`tail -n1 /tmp/lastfailure`)/60 "
+		if [ $lastfailure -ge 30 ];
+		then
+			date +%s >> /tmp/lastfailure
+			echo "Last failure was $lastfailure minutes ago. Restarting."
+			# Turn the HS100 off, wait 10 seconds, then turn it back on
+			# Wait 10 minutes because it takes a while for my internet
+			# to come back up.
+			# Shout out to ggeorgovassilis for the hs100.sh scripts:
+			# https://github.com/ggeorgovassilis/linuxscripts/blob/master/tp-link-hs100-smartplug/hs100.sh
+			/usr/local/bin/hs100.sh 192.168.1.60 9999 off
+			sleep 10s
+			/usr/local/bin/hs100.sh 192.168.1.60 9999 on
+			sleep 10m
+		else
+			echo "Last failure was $lastfailure minutes ago. Not restarting."
+		fi
 	fi
 else
 	if [ $failures -ne 0 ];
